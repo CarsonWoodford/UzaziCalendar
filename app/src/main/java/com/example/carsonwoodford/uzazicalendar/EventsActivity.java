01bb406403de1651683ae47059a2990e5f340ce7
Carson Woodford <CarsonWoodford@HT.local>
2017-05-28 20:21:31 -0600

    Initial commit

---
+++ app/src/main/java/com/example/carsonwoodford/uzazicalendar/EventsActivity.java
@@ -0,0 +1,15 @@
+package com.example.carsonwoodford.uzazicalendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events);
+    }
+
+    //Code to import and show events in a calendar go here.
+}
---
+++ app/src/main/java/com/example/carsonwoodford/uzazicalendar/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.carsonwoodford.uzazicalendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //function for the Events/Calendar button goes here
+
+    //Function for the Donation button goes here.
+}
