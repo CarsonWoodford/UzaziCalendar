b239fcad7ee2069c4c7e1149a58684630dc35a55
heather <hmbee3@gmail.com>
2017-05-30 22:38:14 -0500

    Practice comment merge conflict2

--- app/src/main/java/com/example/carsonwoodford/uzazicalendar/MainActivity.java
+++ app/src/main/java/com/example/carsonwoodford/uzazicalendar/MainActivity.java
@@ -14,5 +14,5 @@ public class MainActivity extends AppCompatActivity {
     //function for the Events/Calendar button goes here
 
     //Function for the Donation button goes here.
-    //This is Heather's comment
+    //Heathers 2nd
 }
