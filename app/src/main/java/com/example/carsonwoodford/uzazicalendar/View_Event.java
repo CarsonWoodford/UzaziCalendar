b8adb4c8bd7131c69ca7779ac0ad89465a3bb553
heather <hmbee3@gmail.com>
2017-05-31 20:44:43 -0500

    Added View_Events

---
+++ app/src/main/java/com/example/carsonwoodford/uzazicalendar/View_Event.java
@@ -0,0 +1,13 @@
+package com.example.carsonwoodford.uzazicalendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class View_Event extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view__event);
+    }
+}
