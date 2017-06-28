1125fbeb5db6db8fd9a281ad2f8454ffd4e7732c
marcushedgecock <hed15001@byui.edu>
2017-06-27 20:20:11 -0600

    javadocs for customEvent class

--- app/src/main/java/com/example/carsonwoodford/uzazicalendar/customEvent.java
+++ app/src/main/java/com/example/carsonwoodford/uzazicalendar/customEvent.java
@@ -6,7 +6,13 @@ import android.os.Parcelable;
 
 import java.sql.Time;
 import java.util.Date;
-
+/**
+ * Represents an event.
+ * <p>
+ *     This class has variables and methods that are
+ *     used for holding information about an event.
+ * </p>
+*/
 public class customEvent implements Parcelable{
 
     private String name; //event name
@@ -16,6 +22,9 @@ public class customEvent implements Parcelable{
     private String desc; //event description
     private String ppl;  //event participants
 
+    /**
+     * Default constructor.
+     */
     customEvent(){
         name = "";
         time = 0;
@@ -24,6 +33,12 @@ public class customEvent implements Parcelable{
         ppl = "";
     }
 
+    /**
+     * A Non-default constructor
+     * @param inputName the name of the event entered by user
+     * @param inputDesc the description of the event entered by user
+     * @param inputTime the time of the event entered by user
+     */
     customEvent(String inputName, String inputDesc, long inputTime){
         name = inputName;
         desc = inputDesc;
@@ -32,10 +47,18 @@ public class customEvent implements Parcelable{
         ppl = "";
     }
 
+    /**
+     * getter that gets event name
+     * @return returns the name of the event object
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * setter that sets the event name
+     * @param name the name of the event
+     */
     public void setName(String name) {
         this.name = name;
     }
@@ -48,6 +71,10 @@ public class customEvent implements Parcelable{
         this.date = date;
     }*/
 
+    /**
+     * getter that gets the event time
+     * @return event time
+     */
     public long getTime() {
         return time;
     }
@@ -85,7 +112,10 @@ public class customEvent implements Parcelable{
         return 0;
     }
 
-
+    /**
+     * A Non-default constructor
+     * @param in an object which is a container for a message
+     */
     protected customEvent(Parcel in) {
         name = in.readString();
         time = in.readLong();
@@ -94,6 +124,11 @@ public class customEvent implements Parcelable{
         ppl = in.readString();
     }
 
+    /**
+     * Writes to a parcel, which is a container for a message
+     * @param dest Parcel received which function will write to
+     * @param flags
+     */
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(name);
